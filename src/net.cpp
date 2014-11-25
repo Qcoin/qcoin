af597bcd5938929d499955e41b6fa85fa144b8a0
Simon <contact.qcoin@gmail.com>
2014-11-25 10:39:39 +0100

    small changes

--- src/net.cpp
+++ src/net.cpp
@@ -1173,7 +1173,7 @@ void MapPort(bool)
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strMainNetDNSSeed[][2] = {
-    {"109.190.164.145"},
+    {"qcointool.org", "dnsseed.qcointool.org"},
     {NULL, NULL}
 };
 
